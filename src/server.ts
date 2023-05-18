872eee905aa78c1b966903ad3ce4f352bb224e77
zaidohdDev <danfealmeida@gmail.com>
2023-05-18 18:16:02 -0300

    config initial spacetime server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users.map((name) => name.name)
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('🚀HTTP server running on http://localhost:3333'))
